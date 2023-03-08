c3f08a744c73d11e82c9c428c23e10d75c5d916a
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-08 15:17:41 +0100

    [add] push completed 1st exercise with no bonus

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -1,7 +1,44 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class TrainTicket {
     public static void main(String[] args) {
 
+        // DATA
+        int userKilometers;
+        int userAge;
+
+        double priceForKm = 0.21;
+
+        // INPUT
+        // con lo scanner vado ad interagire con l'utente permettendogli di inserire i propri dati
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Quanti Km vorresti percorrere?: ");
+        userKilometers = Integer.parseInt(scan.nextLine());
+        System.out.println("Inserisci la tua età: ");
+        userAge = Integer.parseInt(scan.nextLine());
+        // OUTPUT
+
+        // definisco prima il prezzo totale del biglietto (moltiplicando il prezzo per km con i km inseriti dall'utente)
+        double totalTripPrice = priceForKm * userKilometers;
+        System.out.println("Prezzo totale Biglietto: €" + totalTripPrice);
+
+        // creo una condizione che definisce i due sconti in base all'età
+        if (userAge < 18 ){
+
+            //calcolo lo a quanto ammonta lo sconto
+            double discount = (totalTripPrice * 20) / 100;
+            // sottraggo lo sconto al prezzo totale e così ottengo il prezzo scontato
+            double discountedPrice = totalTripPrice - discount;
+            System.out.println("prezzo scontato under 18: " + discountedPrice);
+
+        } else if (userAge >= 65 ) {
+            // // //
+            double discount = (totalTripPrice * 40) / 100;
+            double discountedPrice = totalTripPrice - discount;
+            System.out.println("prezzo scontato over 65: " + discountedPrice);
+
+        }
     }
 }
