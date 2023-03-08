d8326db414309da90661b33dccc9790d55433892
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-08 17:18:11 +0100

    [add] push bonus

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -1,5 +1,6 @@
 package org.lessons.java;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class TrainTicket {
@@ -19,10 +20,10 @@ public class TrainTicket {
         System.out.println("Inserisci la tua età: ");
         userAge = Integer.parseInt(scan.nextLine());
         // OUTPUT
-
+        DecimalFormat decimalFormat = new DecimalFormat("##.##");
         // definisco prima il prezzo totale del biglietto (moltiplicando il prezzo per km con i km inseriti dall'utente)
         double totalTripPrice = priceForKm * userKilometers;
-        System.out.println("Prezzo totale Biglietto: €" + totalTripPrice);
+        System.out.println("Prezzo totale Biglietto: €" + decimalFormat.format(totalTripPrice));
 
         // creo una condizione che definisce i due sconti in base all'età
         if (userAge < 18 ){
@@ -31,13 +32,13 @@ public class TrainTicket {
             double discount = (totalTripPrice * 20) / 100;
             // sottraggo lo sconto al prezzo totale e così ottengo il prezzo scontato
             double discountedPrice = totalTripPrice - discount;
-            System.out.println("prezzo scontato under 18: " + discountedPrice);
+            System.out.println("prezzo scontato under 18: " + decimalFormat.format(discountedPrice));
 
         } else if (userAge >= 65 ) {
             // // //
             double discount = (totalTripPrice * 40) / 100;
             double discountedPrice = totalTripPrice - discount;
-            System.out.println("prezzo scontato over 65: " + discountedPrice);
+            System.out.println("prezzo scontato over 65: " + decimalFormat.format(discountedPrice));
 
         }
     }
