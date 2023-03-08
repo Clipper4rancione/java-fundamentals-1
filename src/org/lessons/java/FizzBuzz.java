26140c3cea03923e0823f53d3326f81c49e3a4e5
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-08 16:28:42 +0100

    [add] push completed 2nd exercise with no bonus

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,7 +1,30 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
+    // INPUT
+        // chiedo all'utente di inserire il numero
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Inserisci un numero compreso tra 0 e 999");
+        int numberLimit = Integer.parseInt(scan.nextLine());
+    // OUTPUT
+        //creo un ciclo che stampa tutti i numeri
+        for (int i = 0; i <= numberLimit; i++){
+            String message = Integer.toString(i);
 
+            if (i % 3 == 0){
+                message = "Fizz";
+            }
+            if(i % 5 == 0){
+                message = "Buzz";
+            }
+            if (i % 15 == 0){
+                message = "FizzBuzz";
+            }
+            System.out.println(message);
+
+        }
     }
 }
